563b3d6f7083e41e77e750b7fc449045257a3fbc
Gael Hugo <gael.hugo@onemore-studio.com>
2020-12-25 16:19:21 +0100

    update config.js

--- 2021/DragElement/config/config.js
+++ 2021/DragElement/config/config.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBCboafcPQ_3ZRI1Sf_00MSpl_SURHWnOI",
-  authDomain: "jeudiecal.firebaseapp.com",
-  databaseURL: "https://jeudiecal.firebaseio.com",
-  projectId: "jeudiecal",
-  storageBucket: "jeudiecal.appspot.com",
-  messagingSenderId: "867653886807",
-  appId: "1:867653886807:web:5652a4cbdda0fe1de75412",
+  apiKey: "***",
+  authDomain: "***",
+  databaseURL: "***",
+  projectId: "***",
+  storageBucket: "***",
+  messagingSenderId: "***",
+  appId: "***",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
